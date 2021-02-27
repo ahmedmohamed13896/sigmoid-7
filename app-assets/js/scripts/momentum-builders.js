47e152cae081dffdf0ce158eae322da3dbb836bc
ahmed mohamed <ahmedmohamed77765@hotmail.com>
2021-02-27 04:48:18 +0200

    Update chart in app-moment-builders

--- app-assets/js/scripts/momentum-builders.js
+++ app-assets/js/scripts/momentum-builders.js
@@ -12,68 +12,17 @@ $(window).on("load", function () {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  function getJsonData(data, elementID) {
+
+
+  function getJsonData(data, elementID ,chartID) {
     let properties = [];
     let tableBody = "";
-
+    let chartDates = [];
+    let chartDatesValues = [];
     let dataHTML = `
                     <thead>
                         <tr>`;
-
-    // store data properties
-    for (let i in data.data[0]) {
-      if (
-        i != "No." &&
-        i != "No" &&
-        i != "Industry" &&
-        i != "Country" &&
-        i != "P/E" &&
-        i != "Id"
-      ) {
-        properties.push(i);
-        dataHTML += `<th>${i}</th>`;
-      }
-    }
-
-    dataHTML += `</tr>
-                    </thead>
-                    <tbody>`;
-
-    $.each(data.data, function (key, value) {
-      tableBody += `
-                        <tr>`;
-      for (let i = 0; i < properties.length; i++) {
-        if (
-          i != "No." &&
-          i != "No" &&
-          i != "Industry" &&
-          i != "Country" &&
-          i != "P/E" &&
-          i != "Id"
-        ) {
-          if (value[properties[i]][0] == "-") {
-            tableBody += `<td class="text-danger">${value[properties[i]]}</td>`;
-          } else if (value[properties[i]] == "") {
-            tableBody += `<td>${numberWithCommas(value[properties[i]])}</td>`;
-          } else if (value[properties[i]]) {
-            tableBody += `<td>${value[properties[i]]}</td>`;
-          }
-        }
-      }
-      tableBody += `
-                           </tr>`;
-    });
-    tableBody += `</tbody>`;
-    dataHTML += tableBody;
-    $(elementID).append(dataHTML);
-  }
-
-  function getJsonData2(data, elementID) {
-    let properties = [];
-    let tableBody = "";
-    let dataHTML = `
-                    <thead>
-                        <tr>`;
+    
 
     // store data properties
     for (let i in data.data[0]) {
@@ -87,14 +36,18 @@ $(window).on("load", function () {
                     </thead>
                     <tbody>`;
 
+
     $.each(data.data, function (key, value) {
       tableBody += `
                            <tr>`;
       for (let i = 0; i < properties.length; i++) {
         if (i != "exchange" && i != "No" && i != "urls" && i != "MC") {
-           value[properties[i]][0] =='-' ?  tableBody += `<td class="text-danger">${value[properties[i]]}</td>`: tableBody += `<td>${value[properties[i]]}</td>`;
+            value[properties[i]][0] =='-' ?  tableBody += `<td class="text-danger">${value[properties[i]]}</td>`: tableBody += `<td>${value[properties[i]]}</td>`;
+          }
         }
-      }
+
+        
+       
       tableBody += `
                            </tr>`;
     });
@@ -102,251 +55,158 @@ $(window).on("load", function () {
     tableBody += `</tbody>`;
     dataHTML += tableBody;
     $(elementID).append(dataHTML);
-  }
 
- 
 
-  function createLineChart(id) {
-    var lineChartEx = $(id),
-     chartWrapper = $('.chartjs');
-    var warningColorShade = "#ffe802",
-      tooltipShadow = "rgba(0, 0, 0, 0.25)",
-      lineChartPrimary = "#666ee8",
-      lineChartDanger = "#ff4961",
-      labelColor = "#6e6b7b",
-      grid_line_color = "rgba(200, 200, 200, 0.2)"; // RGBA color helps in dark layout
 
-    // Line Chart
-    // --------------------------------------------------------------------
 
-    // Wrap charts with div of height according to their data-height
-    if (chartWrapper.length) {
-      chartWrapper.each(function () {
-        $(this).wrap(
-          $(
-            '<div style="height:' +
-              this.getAttribute("data-height") +
-              'px"></div>'
-          )
-        );
-      });
+    // console.log(properties);
+
+   
+
+    for(let i=1; i<properties.length - 1; i++){
+      chartDates.push(properties[i]);
+      // console.log(data.data[0][properties[i]]);
+      chartDatesValues.push(data.data[0][properties[i]]);
     }
 
-    if (lineChartEx.length) {
-      var lineExample = new Chart(lineChartEx, {
-        type: "line",
-        plugins: [
-          // to add spacing between legends and chart
-          {
-            beforeInit: function (chart) {
-              chart.legend.afterFit = function () {
-                this.height += 20;
-              };
-            },
-          },
-        ],
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          backgroundColor: false,
-          hover: {
-            mode: "label",
-          },
-          tooltips: {
-            // Updated default tooltip UI
-            shadowOffsetX: 1,
-            shadowOffsetY: 1,
-            shadowBlur: 8,
-            shadowColor: tooltipShadow,
-            backgroundColor: window.colors.solid.white,
-            titleFontColor: window.colors.solid.black,
-            bodyFontColor: window.colors.solid.black,
-          },
-          layout: {
-            padding: {
-              top: -15,
-              bottom: -25,
-              left: -15,
-            },
-          },
-          scales: {
-            xAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: true,
-                },
-                gridLines: {
-                  display: true,
-                  color: grid_line_color,
-                  zeroLineColor: grid_line_color,
-                },
-                ticks: {
-                  fontColor: labelColor,
-                },
-              },
-            ],
-            yAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: true,
-                },
-                ticks: {
-                  stepSize: 100,
-                  min: 0,
-                  max: 400,
-                  fontColor: labelColor,
-                },
-                gridLines: {
-                  display: true,
-                  color: grid_line_color,
-                  zeroLineColor: grid_line_color,
-                },
-              },
-            ],
-          },
-          legend: {
-            position: "top",
-            align: "start",
-            labels: {
-              usePointStyle: true,
-              padding: 25,
-              boxWidth: 9,
-            },
-          },
-        },
-        data: {
-          labels: [
-            "7/12",
-            "8/12",
-            "9/12",
-            "10/12",
-            "11/12",
-            "12/12",
-            "13/12",
-            "14/12",
-            "15/12",
-            "16/12",
-            "17/12",
-          ],
-          datasets: [
-            {
-              data: [
-                275,
-                90,
-                190,
-                205,
-                125,
-                85,
-                55,
-                87,
-                127,
-                150,
-                230,
-                280,
-                190,
-              ],
-              label: "Europe",
-              borderColor: lineChartDanger,
-              lineTension: 0.5,
-              pointStyle: "circle",
-              backgroundColor: lineChartDanger,
-              fill: false,
-              pointRadius: 1,
-              pointHoverRadius: 5,
-              pointHoverBorderWidth: 5,
-              pointBorderColor: "transparent",
-              pointHoverBorderColor: window.colors.solid.white,
-              pointHoverBackgroundColor: lineChartDanger,
-              pointShadowOffsetX: 1,
-              pointShadowOffsetY: 1,
-              pointShadowBlur: 5,
-              pointShadowColor: tooltipShadow,
-            },
-            {
-              data: [
-                80,
-                125,
-                105,
-                130,
-                215,
-                195,
-                140,
-                160,
-                230,
-                300,
-                220,
-                170,
-                210,
-              ],
-              label: "Asia",
-              borderColor: lineChartPrimary,
-              lineTension: 0.5,
-              pointStyle: "circle",
-              backgroundColor: lineChartPrimary,
-              fill: false,
-              pointRadius: 1,
-              pointHoverRadius: 5,
-              pointHoverBorderWidth: 5,
-              pointBorderColor: "transparent",
-              pointHoverBorderColor: window.colors.solid.white,
-              pointHoverBackgroundColor: lineChartPrimary,
-              pointShadowOffsetX: 1,
-              pointShadowOffsetY: 1,
-              pointShadowBlur: 5,
-              pointShadowColor: tooltipShadow,
-            },
-            {
-              data: [
-                80,
-                99,
-                82,
-                90,
-                115,
-                115,
-                74,
-                75,
-                130,
-                155,
-                125,
-                90,
-                140,
-              ],
-              label: "Africa",
-              borderColor: warningColorShade,
-              lineTension: 0.5,
-              pointStyle: "circle",
-              backgroundColor: warningColorShade,
-              fill: false,
-              pointRadius: 1,
-              pointHoverRadius: 5,
-              pointHoverBorderWidth: 5,
-              pointBorderColor: "transparent",
-              pointHoverBorderColor: window.colors.solid.white,
-              pointHoverBackgroundColor: warningColorShade,
-              pointShadowOffsetX: 1,
-              pointShadowOffsetY: 1,
-              pointShadowBlur: 5,
-              pointShadowColor: tooltipShadow,
-            },
-          ],
-        },
-      });
+    let sum = chartDatesValues.reduce(function (a, b) {
+      return Number(a) + Number(b);
+    }, 0);     
+
+    for(let x=0; x<chartDatesValues.length; x++){
+      chartDatesValues[x] = ((chartDatesValues[x] * 100) / sum).toFixed(2) ;
+      console.log(chartDatesValues[x] + "%");
     }
+
+    
+
+
+    createBarChart(chartID, chartDates, chartDatesValues);
+
   }
+
+    function createBarChart(id, chartDates, chartDatesValues) {
+      var barChartEx = $(id),
+        chartWrapper = $(".chartjs");
+      var warningColorShade = "#ffe802",
+        tooltipShadow = "rgba(0, 0, 0, 0.25)",
+        lineChartPrimary = "#666ee8",
+        lineChartDanger = "#ff4961",
+        labelColor = "#6e6b7b",
+        successColorShade = "#28dac6",
+        grid_line_color = "rgba(200, 200, 200, 0.2)"; // RGBA color helps in dark layout
+
+      // for(let i=0;i<chartDatesValues.length;i++){
+      //   chartDatesValues+= "%";
+      // }
+
+
+      // Line Chart
+      // --------------------------------------------------------------------
+
+      // Wrap charts with div of height according to their data-height
+      if (chartWrapper.length) {
+        chartWrapper.each(function () {
+          $(this).wrap(
+            $(
+              '<div style="height:' +
+                this.getAttribute("data-height") +
+                'px"></div>'
+            )
+          );
+        });
+      }
+
+        if (barChartEx.length) {
+          var barChartExample = new Chart(barChartEx, {
+            type: "bar",
+            options: {
+              elements: {
+                rectangle: {
+                  borderWidth: 2,
+                  borderSkipped: "bottom",
+                },
+              },
+              responsive: true,
+              maintainAspectRatio: false,
+              responsiveAnimationDuration: 500,
+              legend: {
+                display: false,
+              },
+              tooltips: {
+                // Updated default tooltip UI
+                shadowOffsetX: 1,
+                shadowOffsetY: 1,
+                shadowBlur: 8,
+                shadowColor: tooltipShadow,
+                backgroundColor: window.colors.solid.white,
+                titleFontColor: window.colors.solid.black,
+                bodyFontColor: window.colors.solid.black,
+              },
+              scales: {
+                xAxes: [
+                  {
+                    barThickness: 15,
+                    display: true,
+                    gridLines: {
+                      display: true,
+                      color: grid_line_color,
+                      zeroLineColor: grid_line_color,
+                    },
+                    scaleLabel: {
+                      display: false,
+                    },
+                    ticks: {
+                      fontColor: labelColor,
+                    },
+                  },
+                ],
+                yAxes: [
+                  {
+                    display: true,
+                    gridLines: {
+                      color: grid_line_color,
+                      zeroLineColor: grid_line_color,
+                    },
+                    ticks: {
+                      stepSize: 20,
+                      min: id=="#winners_line_chart"? 0: -100,
+                      max: 100,
+                      fontColor: labelColor,
+                      callback: function (label) {
+                        if (label) {
+                          return label + "%";
+                        }
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+            data: {
+              labels: chartDates,
+              datasets: [
+                {
+                  data: chartDatesValues,
+                  backgroundColor: successColorShade,
+                  borderColor: "transparent",
+                },
+              ],
+            },
+          });
+        }
+    }
 
   // get data from the json link
 
   // Momentum Builders 3D winners
-    fetch("https://api.apispreadsheets.com/data/8617/")
+    fetch("https://api.apispreadsheets.com/data/8620/")
     .then((response) => response.json())
     .then((data) => {
         // Create Table from Json
-        var status = "loser";
 
-        getJsonData2(data, "#3DWinners");
-        createLineChart("#winners_line_chart");
+        getJsonData(data, "#3DWinners", "#winners_line_chart");
+        // createLineChart();
     });
 
   // Momentum Builders 3D Losers
@@ -354,9 +214,9 @@ $(window).on("load", function () {
     .then((response) => response.json())
     .then((data) => {
       // Create Table from Json
-      var status = "loser";
+      
 
-      getJsonData2(data, "#3DLosers");
-      createLineChart("#losers_line_chart");
+      getJsonData(data, "#3DLosers", "#losers_line_chart");
+      // createLineChart();
     });
 });
